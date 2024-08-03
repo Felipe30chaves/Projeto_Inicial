b56c7f2cbc1bb4326a5d0e897724fa84e101cb28
FELIPE MENEZES CHAVES <fengmenezes@gmail.com>
2024-08-02 22:56:49 -0300

    meu primeiro commit

---
+++ PROJETO/main.py
@@ -0,0 +1,18 @@
+print("Jornada dos dados")
+
+#Variaveis
+
+idade = 31
+nome = "Felipe"
+
+# print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+
+#listas
+lista = [1,2,3,4,5]
+
+#metodos
+lista_invertida = lista.reverse()
